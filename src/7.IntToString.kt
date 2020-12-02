4e570374f14d0d2c27712631a4c5249f248e6d2b
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2020-12-02 13:44:00 +0545

    Solution 7

---
+++ src/7.IntToString.kt
@@ -0,0 +1,29 @@
+package assignment1
+
+fun main(){
+    print("Enter a number :")
+    val num:Int= readLine()!!.toInt()
+
+     when(num){
+         1 ->
+             print("One")
+         2 ->
+             print("Two")
+         3 ->
+             print("Three")
+         4 ->
+             print("Four")
+         5 ->
+             print("Five")
+         6 ->
+             print("Six")
+         7 ->
+             print("Seven")
+         8 ->
+             print("Eight")
+         9 ->
+             print("Nine")
+     }
+}
+
+
