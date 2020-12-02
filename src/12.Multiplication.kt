b74aa30d8ccbd2cf0823069eb732761e5ec3963e
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2020-12-02 13:46:56 +0545

    Solution 12

---
+++ src/12.Multiplication.kt
@@ -0,0 +1,11 @@
+package assignment1
+
+fun main(){
+    print("Enter the number to find the multiplication table :")
+    val num: Int= readLine()!!.toInt()
+
+    for (i: Int in 1..10) {
+        val multiply = num * i
+        println("$num * $i = $multiply")
+    }
+}
